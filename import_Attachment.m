9350404e8be40743d42b33c36351b1b4a83b42f3
GGQ@SB1703 <gqguan@hotmail.com>
2020-09-13 23:12:53 +0800

    初次提交

---
+++ import_Attachment.m
@@ -0,0 +1,38 @@
+function [Mails,allOK] = import_Attachment(Mails)
+% 检查输入参数
+if nargin ~= 1 || ~isstruct(Mails)
+    fprintf('[错误]参数输入错误！\n');
+    return
+end
+% 初始化
+allOK = true;
+% 选取附件文件（多个）
+[Files, Path] = uigetfile('*.*', '选取附件文件 ...', 'Multiselect', 'on');
+Files = cellstr(Files);
+Path = cellstr(Path);
+FullPaths = strcat(Path, Files);
+% 按名单查找所选附件
+for i = 1:length(Mails)
+    idxFile = cellfun(@(x)(contains(x,Mails(i).Recipient.Name)),Files);
+    text = sprintf('%s（%s）同学：\n\n你好！\n',Mails(i).Recipient.Name,Mails(i).Recipient.SN);
+    if sum(idxFile) == 1
+        Mails(i).Attachment = FullPaths(idxFile);
+        text = [text,sprintf('根据你的要求，现将批改后的课程报告（详见附件）返回给你，请查收！\n\n')];
+        ok = true;
+    else
+        if sum(idxFile) == 0
+            Mails(i).Remarks = sprintf('[警告]没有发现附件文件与%s匹配。\n', Mails(i).Recipient.Name);
+            text = [text,sprintf('没有发现你提交的课程报告，请与我联系!\n\n')];
+            ok = true;
+        else
+            fprintf('[警告]发现存在多个附件文件与%s匹配。\n', Mails(i).Recipient.Name)
+            ok = false;
+            allOK = false;
+        end
+    end
+    if ok
+        text = [text,sprintf('礼\n\n关国强\n\n')];
+        text = [text,sprintf('邮件内容生成程序（https://github.com/gqguan/EMailDistributor），%s',char(datetime('now')))];
+        Mails(i).Content = text;
+    end
+end
---
+++ import_Feedback.m
@@ -0,0 +1,43 @@
+function data = import_Feedback(workbookFile, sheetName, range)
+%IMPORTFILE 导入电子表格中的数据
+%   DATA = IMPORTFILE(FILE) 读取名为 FILE 的 Microsoft Excel
+%   电子表格文件的第一张工作表中的数据，并以元胞数组的形式返回该数据。
+%
+%   DATA = IMPORTFILE(FILE,SHEET) 从指定的工作表中读取。
+%
+%   DATA = IMPORTFILE(FILE,SHEET,RANGE) 从指定的工作表和指定的范围中读取。使用语法 'C1:C2'
+%   指定范围，其中 C1 和 C2 是区域的对角。%
+% 示例:
+%   Untitled = importfile('能源化工专业“前沿讲座”课程意见反馈调查(1-26).xlsx','Sheet1','A2:P27');
+%
+%   另请参阅 XLSREAD。
+
+% 由 MATLAB 自动生成于 2020/09/13 11:53:18
+
+%% 输入处理
+
+% 若没指定workbookFile，则打开文件对话框
+if nargin == 0 
+    [workbookFile, workbookPath] = uigetfile('*.*', '选取意见反馈调查结果Excel工作簿 ...', 'Multiselect', 'off');
+end
+
+% 如果未指定工作表，则将读取第一张工作表
+if nargin == 1 || ~exist('sheetName','var')
+    sheetName = 1;
+end
+
+% 如果未指定范围，则将读取所有数据
+if nargin <= 2 || ~exist('range','var')
+    range = '';
+end
+
+%% 导入数据
+[~, ~, data] = xlsread(workbookFile, sheetName, range);
+data(cellfun(@(x) ~isempty(x) && isnumeric(x) && isnan(x),data)) = {''};
+
+idx = cellfun(@ischar, data);
+data(idx) = cellfun(@(x) string(x), data(idx), 'UniformOutput', false);
+% 删除表头，只保留姓名、学号和邮件信息
+data = data(2:end,[6 7 10]);
+% 删除空邮件地址的行
+data = data(cellfun(@(x)(x~=""),data(:,3)),:);
